ad31e278a8c3cf6ed3f704d7b4b69069a97fcc94
Aycan <aycnkya12@gmail.com>
2022-07-20 18:14:17 +0300

    defined generic method

---
+++ src/api/service/UserService.tsx
@@ -0,0 +1,35 @@
+import LoginForm from "../../routes/Login/LoginForm/LoginForm";
+import RegisterForm from "../../routes/Register/RegisterForm/RegisterForm";
+import { LoginValues } from "../../routes/Login/LoginForm/LoginForm";
+import { RegisterValues } from "../../routes/Register/RegisterForm/RegisterForm";
+import { useNavigate } from "react-router-dom";
+import { post, get } from "../../api/axios";
+
+export default function Login() {
+  let navigate = useNavigate();
+
+  async function handleSubmit(values: LoginValues) {
+    const user = await post("/Account/authenticate", values);
+    localStorage.setItem("jwToken", user.jwToken);
+    if (await get("/Account/isValid")) {
+      navigate("../");
+    }
+  }
+  return <LoginForm onSubmit={handleSubmit} />;
+}
+
+export function Register() {
+  let navigate = useNavigate();
+  if (localStorage.getItem("jwToken")) {
+    return <div>giriş yapmışssın</div>;
+  }
+
+  async function handleSubmit(values: RegisterValues) {
+    const response = await post("/Account/register", values);
+    if (response) {
+      navigate("../login");
+    }
+  }
+
+  return <RegisterForm onSubmit={handleSubmit} />;
+}
