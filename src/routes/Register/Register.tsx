be200e71a28a59d32d8199792e7bb2a4c7e5f6fc
Aycan <aycnkya12@gmail.com>
2022-07-20 15:37:15 +0300

    navigate added

--- src/routes/Register/Register.tsx
+++ src/routes/Register/Register.tsx
@@ -4,17 +4,17 @@ import { RegisterValues } from "./RegisterForm/RegisterForm";
 import { useNavigate } from "react-router-dom";
 
 export default function Register() {
+  let navigate = useNavigate();
   if (localStorage.getItem("jwToken")) {
     return <div>giriş yapmışssın</div>;
   }
-  let navigate = useNavigate();
 
   async function handleSubmit(values: RegisterValues) {
     const response = await register(values);
-    console.log(response);
+    console.log(navigate);
     if (response) {
       console.log(navigate);
-      navigate("/login");
+      navigate("../login");
     }
   }
 
