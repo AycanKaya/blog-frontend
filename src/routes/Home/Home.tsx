1aab2c609ad1668dd2856b46eebddc6d753fc4b0
Aycan <aycnkya12@gmail.com>
2022-07-21 15:55:03 +0300

    app bar added

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,14 +1,38 @@
 import { Link } from "react-router-dom";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function Home() {
   return (
-    <>
-      <div>
-        <Link to="register">üye ol</Link>
-      </div>
-      <div>
-        <Link to="login">giriş yap</Link>
-      </div>
-    </>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Link to="register">
+            <Button color="inherit">Sign Up</Button>
+          </Link>
+          <Link to="login">
+            <Button color="inherit">Login</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
