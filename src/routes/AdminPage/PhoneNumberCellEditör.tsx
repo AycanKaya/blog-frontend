b24988107f3e53381acf5a53d7c33f4fb1d81982
Aycan <aycnkya12@gmail.com>
2022-07-26 17:21:39 +0300

    Phone Cell Editör

---
+++ src/routes/AdminPage/PhoneNumberCellEditör.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+export default function PhoneNumberCellEditör(params: any) {
+  function handleChange(event: any) {
+    params.phoneNumber = event.target.value;
+  }
+
+  return (
+    <Box
+      component="form"
+      sx={{
+        "& .MuiTextField-root": { m: 1, width: "15ch" },
+      }}
+      noValidate
+      autoComplete="on"
+    >
+      <div>
+        <TextField
+          required
+          id="outlined-required"
+          label=""
+          defaultValue={params.phoneNumber}
+          //  value={event?.target.value}
+          onChange={(event) => handleChange(event)}
+        />
+      </div>
+    </Box>
+  );
+}
