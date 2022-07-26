434e49da365f27f21e892e1357dedf0d1b373b8b
Aycan <aycnkya12@gmail.com>
2022-07-26 11:31:53 +0300

    updateUser request added

--- src/routes/AdminPage/AgGrid.tsx
+++ src/routes/AdminPage/AgGrid.tsx
@@ -3,10 +3,9 @@ import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/dist/styles/ag-grid.css";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import "ag-grid-community/dist/styles/ag-theme-alpine.css";
-import { get } from "../../api/axios";
+import { get, post, put } from "../../api/axios";
 import SaveIcon from "@mui/icons-material/Save";
 import { Button, FormControl, InputLabel, MenuItem } from "@mui/material";
-import PropTypes from "prop-types";
 
 interface IRow {
   userID: string;
@@ -15,7 +14,7 @@ interface IRow {
   surname: string;
   role: string;
   gender: string;
-  birthDay: string;
+  birthDay: Date;
   age: string;
   phoneNumber: string;
   contry: string;
@@ -27,12 +26,17 @@ export default function GenerateRows() {
 
   function getAllUserInfo() {
     get("/Account/GetAllUserInfo").then((response: IRow[]) => {
-      console.log("RESPONSE ", response);
       setRowArray(response);
     });
   }
 
-  useEffect(() => console.log("tekrar sey yapicaz", rowArray), [rowArray]);
+  function updateUser(userId: string, body: string) {
+    const url = "/Admin/Dashboard?id=" + userId;
+    put(url, body).then((respone: any) => {
+      console.log(respone);
+    });
+  }
+
   useEffect(() => {
     getAllUserInfo();
   }, []);
@@ -41,23 +45,21 @@ export default function GenerateRows() {
     var year = strDate.substr(0, 4);
     var mounth = strDate.substr(5, 2);
     var day = strDate.substr(8, 2);
-    return year + "/" + mounth + "/" + day;
+    return year + "-" + mounth + "-" + day;
   }
 
   rowArray?.forEach(function (value) {
-    value.birthDay = convertDate(value.birthDay);
+    // value.birthDay = convertDate(value.birthDay.toString());
   });
 
   const columnDefs = [
     { field: "userName", editable: true },
     { field: "name" },
-    { field: "surname" },
+    { field: "surname", editable: true },
     {
       field: "role",
       editable: true,
       cellRenderer: (params: any) => {
-        console.log("params***************");
-        console.log(params);
         return userRoleCellRenderer(params.data);
       },
     },
@@ -73,7 +75,9 @@ export default function GenerateRows() {
         return (
           <Button
             color="secondary"
-            onClick={(event: any) => handleUserUpdate(event, params.node.data)}
+            onClick={(event: any) =>
+              handleUserUpdate(event, params.node.data.userID)
+            }
             startIcon={<SaveIcon />}
             variant="contained"
           >
@@ -83,18 +87,24 @@ export default function GenerateRows() {
       },
     },
   ];
+  function handleUserUpdate(event: any, userId: string) {
+    rowArray.map((row: any) => {
+      if (row.userID === userId) {
+        const newarray = row;
+        const { userID, ...requestBody } = newarray;
+        console.log(requestBody);
+        updateUser(userId, requestBody);
+      }
+    });
+  }
 
   const handleChange = (
     event: SelectChangeEvent,
     params: any,
     rowArray: any
   ) => {
-    console.log("ROW ARRAY:");
-    console.log(rowArray);
     setRowArray(
       rowArray.map((row: any) => {
-        console.log(row.userID);
-        console.log(params.userID);
         if (row.userID === params.userID) {
           return {
             ...row,
@@ -107,8 +117,6 @@ export default function GenerateRows() {
   };
 
   function userRoleCellRenderer(params: any) {
-    console.log("UserRoleRenderer: " + rowArray);
-    console.log(rowArray);
     return (
       <>
         <FormControl fullWidth>
@@ -129,8 +137,6 @@ export default function GenerateRows() {
     );
   }
 
-  function handleUserUpdate(event: any, data: any) {}
-  console.log(rowArray);
   return (
     <div className="ag-theme-alpine" style={{ height: 400, width: "100%" }}>
       <AgGridReact
