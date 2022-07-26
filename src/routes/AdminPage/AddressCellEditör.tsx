4b20064e8cd815e7315b938cbeb8ccbed43b94b3
Aycan <aycnkya12@gmail.com>
2022-07-26 17:20:42 +0300

    Address Cell Editör

---
+++ src/routes/AdminPage/AddressCellEditör.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+export default function AddressCellEditör(params: any) {
+  const [value, setValue] = React.useState("Controlled");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    params.address = event.target.value;
+    setValue(event.target.value);
+  };
+
+  return (
+    <Box
+      component="form"
+      sx={{
+        "& .MuiTextField-root": { m: 1, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          id="outlined-multiline-static"
+          label="Multiline"
+          multiline
+          rows={4}
+          defaultValue={params.address}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleChange(event)
+          }
+        />
+      </div>
+    </Box>
+  );
+}
