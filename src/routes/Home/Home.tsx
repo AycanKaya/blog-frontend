6dc2ecf27ee603cfb06f3f574f4185b570caa5f1
Aycan <aycnkya12@gmail.com>
2022-07-27 18:15:48 +0300

    home edit

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -7,32 +7,71 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import "./Style/style.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+declare module "@mui/material/styles" {
+  interface Palette {
+    neutral: Palette["primary"];
+  }
+
+  // allow configuration using `createTheme`
+  interface PaletteOptions {
+    neutral?: PaletteOptions["primary"];
+  }
+}
+
+// Update the Button's color prop options
+declare module "@mui/material/Button" {
+  interface ButtonPropsColorOverrides {
+    neutral: true;
+  }
+}
 
 export default function Home() {
+  const theme = createTheme({
+    palette: {
+      neutral: {
+        main: "#0a0c0a",
+        contrastText: "#fff",
+      },
+      primary: {
+        light: "#ff7961",
+        dark: "#0a0c0a",
+        main: "#607d8c",
+        contrastText: "#fff",
+      },
+    },
+  });
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Link to="register">
-            <Button color="inherit">Sign Up</Button>
-          </Link>
-          <Link to="login">
-            <Button color="inherit">Login</Button>
-          </Link>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <>
+      <ThemeProvider theme={theme}>
+        <Box sx={{ flexGrow: 2 }}>
+          <AppBar position="static" color="primary">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                News
+              </Typography>
+              <Link to="register">
+                <Button color="neutral">Sign Up</Button>
+              </Link>
+              <Link to="login">
+                <Button color="neutral">Login</Button>
+              </Link>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      </ThemeProvider>
+    </>
   );
 }
