497ac0d38afe5681ca414ced33b46d53dc88e88a
Aycan <aycnkya12@gmail.com>
2022-07-29 01:02:42 +0300

    main account page

--- src/routes/BlogPage/BlogForm/Elements/UserInformation.tsx
+++ src/routes/BlogPage/BlogForm/Elements/UserInformation.tsx
@@ -1,16 +1,85 @@
-import { TextField } from "@mui/material";
-import * as React from "react";
-import "../Styles/style.css";
-const UserInformation = () => {
+import Box from "@mui/material/Box";
+
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import AccountSettings from "./AccountSettings";
+import { get } from "../../../../api/axios";
+import React, { useEffect, useState } from "react";
+import ProfileSettings from "./ProfileSettings";
+
+interface IUser {
+  userID: string;
+  userName: string;
+  name: string;
+  surname: string;
+  role: string;
+  gender: string;
+  birthDay: Date;
+  age: string;
+  phoneNumber: string;
+  contry: string;
+  address: string;
+}
+const defaultUser: IUser = {
+  userID: "",
+  userName: "",
+  name: "",
+  surname: "",
+  role: "",
+  gender: "",
+  birthDay: new Date(""),
+  age: "",
+  phoneNumber: "",
+  contry: "",
+  address: "",
+};
+
+const UserInformaiton: React.FC = () => {
+  const [value, setValue] = useState("1");
+  const [userInfo, setUserInfo] = useState<IUser>(defaultUser);
+
+  async function getUserInfo() {
+    await get("/Account/GetUserInfo").then((response: IUser) => {
+      setUserInfo(response);
+    });
+  }
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
   return (
-    <div className="center">
-      <TextField
-        label="Primary"
-        placeholder="Type in here…"
-        variant="outlined"
-        color="primary"
-      />
-    </div>
+    <Box
+      sx={{
+        width: "auto",
+        typography: "body1",
+        alignItems: "center",
+        paddingLeft: "500px",
+        paddingTop: "50px",
+      }}
+    >
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="Item One" value="1" />
+            <Tab label="Item Two" value="2" />
+            <Tab label="Item Three" value="3" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">
+          <AccountSettings userInfo={userInfo} />
+        </TabPanel>
+        <TabPanel value="2">
+          <ProfileSettings userInfo={userInfo} />
+        </TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+      </TabContext>
+    </Box>
   );
 };
-export default UserInformation;
+export default UserInformaiton;
