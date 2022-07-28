8754b424cb8ef5f0ab062ed8860421fcaa7fc8b1
Aycan <aycnkya12@gmail.com>
2022-07-29 01:02:01 +0300

    accunt information update

---
+++ src/routes/BlogPage/BlogForm/Elements/AccountSettings.tsx
@@ -0,0 +1,61 @@
+import { Button, TextField } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
+import "./style.css";
+import { post } from "../../../../api/axios";
+
+interface IUser {
+  userID: string;
+  userName: string;
+  name: string;
+  surname: string;
+  role: string;
+  gender: string;
+  birthDay: Date;
+  age: string;
+  phoneNumber: string;
+  contry: string;
+  address: string;
+}
+interface UserProps {
+  userInfo: IUser;
+}
+
+const AccountSettings: React.FC<UserProps> = ({ userInfo }) => {
+  const [editable, setEditable] = useState(true);
+
+  const [name, setName] = useState(userInfo.userName);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+  function handleEditableChange() {
+    setEditable(false);
+  }
+
+  return (
+    <>
+      <div>
+        {" "}
+        UserName :
+        <TextField
+          id="standard-required"
+          variant="standard"
+          value={name}
+          disabled={editable}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleChange(event)
+          }
+        />
+        <IconButton aria-label="edit" onClick={handleEditableChange}>
+          <EditIcon />
+        </IconButton>
+      </div>
+      <Button variant="outlined" disabled={editable}>
+        SAVE
+      </Button>
+    </>
+  );
+};
+export default AccountSettings;
