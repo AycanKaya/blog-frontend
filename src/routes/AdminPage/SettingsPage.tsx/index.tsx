c9b77dca60cc257aff2a1c5ea18e674d81f1885b
Aycan <aycnkya12@gmail.com>
2022-07-29 17:46:43 +0300

    setting page

---
+++ src/routes/AdminPage/SettingsPage.tsx/index.tsx
@@ -0,0 +1,12 @@
+import UserInformation from "../../BlogPage/BlogForm/Elements/UserInformation";
+import ResponsiveAppBar from "../AdminResponsiveBar";
+
+const SettingsPage = () => {
+  return (
+    <>
+      <ResponsiveAppBar />
+      <UserInformation />
+    </>
+  );
+};
+export default SettingsPage;
