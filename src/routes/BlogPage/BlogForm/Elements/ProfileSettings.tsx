038c4bb9e780607f94a7251eec7063f4f37e3810
Aycan <aycnkya12@gmail.com>
2022-07-29 13:41:18 +0300

    design changed

--- src/routes/BlogPage/BlogForm/Elements/ProfileSettings.tsx
+++ src/routes/BlogPage/BlogForm/Elements/ProfileSettings.tsx
@@ -1,9 +1,18 @@
-import { Button, TextField } from "@mui/material";
-import React, { useEffect, useState } from "react";
-import IconButton from "@mui/material/IconButton";
-import EditIcon from "@mui/icons-material/Edit";
+import {
+  Button,
+  FormControl,
+  makeStyles,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import React, { useState } from "react";
 import "./style.css";
 import { post } from "../../../../api/axios";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormLabel from "@mui/material/FormLabel";
 
 interface IUser {
   userID: string;
@@ -11,7 +20,7 @@ interface IUser {
   name: string;
   surname: string;
   role: string;
-  gender: string;
+  gender: number;
   birthDay: Date;
   age: string;
   phoneNumber: string;
@@ -20,9 +29,10 @@ interface IUser {
 }
 interface UserProps {
   userInfo: IUser;
+  setUserInfo: React.Dispatch<React.SetStateAction<IUser>>;
 }
 
-const ProfileSettings: React.FC<UserProps> = ({ userInfo }) => {
+const ProfileSettings: React.FC<UserProps> = ({ userInfo, setUserInfo }) => {
   const [editable, setEditable] = useState(true);
 
   const [user, setUser] = useState<IUser>({
@@ -49,10 +59,10 @@ const ProfileSettings: React.FC<UserProps> = ({ userInfo }) => {
     setUser({ ...user, surname: event.target.value });
   };
   const handleGenderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUser({ ...user, gender: event.target.value });
+    setUser({ ...user, gender: Number(event.target.value) });
   };
-  const handleBirthdayChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    // setUser({ ...user, birthDay: event.target.value });
+  const handleBirthdayChange = (event: any) => {
+    setUser({ ...user, birthDay: event.target.value });
   };
   const handleAgeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUser({ ...user, age: event.target.value });
@@ -70,6 +80,14 @@ const ProfileSettings: React.FC<UserProps> = ({ userInfo }) => {
   function handleEditableChange() {
     setEditable(false);
   }
+
+  function setDefaultValue(date: string) {
+    var year = date.substr(0, 4);
+    var mounth = date.substr(5, 2);
+    var day = date.substr(8, 2);
+    return year + "-" + mounth + "-" + day;
+  }
+
   async function updateUser() {
     await post("/Account/UserInfo", user).then((response: IUser) => {
       console.log("Success", response);
@@ -78,15 +96,23 @@ const ProfileSettings: React.FC<UserProps> = ({ userInfo }) => {
 
   async function updateUserInformation() {
     await updateUser();
+    setUserInfo(user);
     setEditable(true);
   }
+  const sx = {
+    paddingInlineStart: "15px",
+    margin: "5px",
+    verticalAlign: "baseline",
+  };
 
   return (
     <>
-      <div>
-        UserName :
+      <div className="borderStyle">
+        <p className="social">USERNAME : </p>
         <TextField
+          sx={sx}
           id="standard-required"
+          InputProps={{ disableUnderline: true }}
           variant="standard"
           value={user.userName}
           disabled={editable}
@@ -94,66 +120,155 @@ const ProfileSettings: React.FC<UserProps> = ({ userInfo }) => {
             handleChange(event)
           }
         />
-        <IconButton aria-label="edit" onClick={handleEditableChange}>
-          <EditIcon />
-        </IconButton>
       </div>
-      <div>
+
+      <div className="borderStyle">
         {" "}
         Name :
         <TextField
+          sx={sx}
           id="standard-required"
           variant="standard"
+          InputProps={{ disableUnderline: true }}
           value={user.name}
           disabled={editable}
           onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
             handleNameChange(event)
           }
         />
-        <IconButton aria-label="edit" onClick={handleEditableChange}>
-          <EditIcon />
-        </IconButton>
       </div>
-      <div>
+
+      <div className="borderStyle">
         {" "}
         Surname :
         <TextField
+          sx={sx}
           id="standard-required"
           variant="standard"
           value={user.surname}
+          InputProps={{ disableUnderline: true }}
           disabled={editable}
           onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
             handleSurnameChange(event)
           }
         />
-        <IconButton aria-label="edit" onClick={handleEditableChange}>
-          <EditIcon />
-        </IconButton>
       </div>
-      <div>
+
+      <div className="borderStyle">
+        <p className="social">GENDER : </p>
+
+        <FormControl>
+          <RadioGroup
+            sx={{ display: "-webkit-box", verticalAlign: "baseline" }}
+            aria-labelledby="demo-controlled-radio-buttons-group"
+            name="controlled-radio-buttons-group"
+            value={user.gender}
+            onChange={(event: any) => handleGenderChange(event)}
+          >
+            <FormControlLabel
+              disabled={editable}
+              value={0}
+              control={<Radio />}
+              label="Kadın"
+            />
+            <FormControlLabel
+              disabled={editable}
+              value={1}
+              control={<Radio />}
+              label="Erkek"
+            />
+          </RadioGroup>
+        </FormControl>
+      </div>
+
+      <div className="borderStyle">
         {" "}
-        Gender :
+        Birthday :
         <TextField
-          id="standard-required"
+          sx={{ width: 220, verticalAlign: "baseline" }}
+          type="date"
           variant="standard"
-          value={user.gender}
+          defaultValue={setDefaultValue(user.birthDay as unknown as string)}
           disabled={editable}
+          InputProps={{ disableUnderline: true }}
           onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-            handleGenderChange(event)
+            handleBirthdayChange(event)
           }
         />
-        <IconButton aria-label="edit" onClick={handleEditableChange}>
-          <EditIcon />
-        </IconButton>
+      </div>
+
+      <div className="borderStyle">
+        {" "}
+        Age :
+        <TextField
+          sx={sx}
+          id="standard-required"
+          variant="standard"
+          value={user.age}
+          disabled={editable}
+          InputProps={{ disableUnderline: true }}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleAgeChange(event)
+          }
+        />
+      </div>
+
+      <div className="borderStyle">
+        {" "}
+        Phone Number :
+        <TextField
+          sx={sx}
+          id="standard-required"
+          variant="standard"
+          value={user.phoneNumber}
+          disabled={editable}
+          InputProps={{ disableUnderline: true }}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handlePhoneChange(event)
+          }
+        />
+      </div>
+
+      <div className="borderStyle">
+        {" "}
+        Country :
+        <TextField
+          sx={sx}
+          id="standard-required"
+          variant="standard"
+          value={user.contry}
+          disabled={editable}
+          InputProps={{ disableUnderline: true }}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleCountryChange(event)
+          }
+        />
+      </div>
+
+      <div className="borderStyle">
+        {" "}
+        Address :
+        <TextField
+          sx={sx}
+          id="standard-required"
+          variant="standard"
+          value={user.address}
+          disabled={editable}
+          InputProps={{ disableUnderline: true }}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleAddressChange(event)
+          }
+        />
       </div>
 
       <Button
         variant="outlined"
-        disabled={editable}
+        disabled={false}
         onClick={updateUserInformation}
       >
         SAVE
       </Button>
+      <Button onClick={handleEditableChange}>UPDATE</Button>
     </>
   );
 };
