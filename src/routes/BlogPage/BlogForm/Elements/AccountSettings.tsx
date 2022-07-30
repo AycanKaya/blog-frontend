c77a746a5be9fcf04a2a357a6b34ce34a590361c
Aycan <aycnkya12@gmail.com>
2022-07-30 23:14:18 +0300

    updated

--- src/routes/BlogPage/BlogForm/Elements/AccountSettings.tsx
+++ src/routes/BlogPage/BlogForm/Elements/AccountSettings.tsx
@@ -4,12 +4,14 @@ import IconButton from "@mui/material/IconButton";
 import EditIcon from "@mui/icons-material/Edit";
 import "./style.css";
 import { post } from "../../../../api/axios";
+import SendIcon from "@mui/icons-material/Send";
 
 interface IUser {
   userID: string;
   userName: string;
   name: string;
   surname: string;
+  email: string;
   role: string;
   gender: number;
   birthDay: Date;
@@ -21,39 +23,125 @@ interface IUser {
 interface UserProps {
   userInfo: IUser;
 }
+interface IRequestPassword {
+  email: string;
+  oldPassword: string;
+  password: string;
+  confirmPassword: string;
+}
+const requestBody: IRequestPassword = {
+  email: "",
+  oldPassword: "",
+  password: "",
+  confirmPassword: "",
+};
 
 const AccountSettings: React.FC<UserProps> = ({ userInfo }) => {
-  const [editable, setEditable] = useState(true);
-
+  const [edit, setEdit] = useState(true);
+  const [body, setBody] = useState(requestBody);
   const [name, setName] = useState(userInfo.userName);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value);
   };
+
   function handleEditableChange() {
-    setEditable(false);
+    setEdit(false);
   }
 
+  function updatePassword() {}
+
+  const sx = {
+    padding: "10px",
+    paddingInlineStart: "10px",
+    borderRadius: "10px",
+    width: 200,
+    boxShadow: "#3B719F 0px 5px 15px",
+    display: "inline-table",
+  };
   return (
     <>
-      <div>
-        {" "}
-        UserName :
+      <div className="textDiv">
+        <p className="social">Email : </p>
         <TextField
+          sx={sx}
           id="standard-required"
           variant="standard"
-          value={name}
-          disabled={editable}
+          size="medium"
+          value={userInfo.email}
+          InputProps={{ disableUnderline: true }}
+          disabled={edit}
           onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
             handleChange(event)
           }
         />
-        <IconButton aria-label="edit" onClick={handleEditableChange}>
-          <EditIcon />
-        </IconButton>
       </div>
-      <Button variant="outlined" disabled={editable}>
+      <div className="textDiv">
+        <p className="social">Old Password : </p>
+        <TextField
+          sx={sx}
+          variant="standard"
+          size="medium"
+          value={userInfo.userName}
+          InputProps={{ disableUnderline: true }}
+          disabled={edit}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleChange(event)
+          }
+        />
+      </div>
+      <div className="textDiv">
+        <p className="social">Password : </p>
+        <TextField
+          sx={sx}
+          id="standard-required"
+          variant="standard"
+          InputProps={{ disableUnderline: true }}
+          value={userInfo.name}
+          disabled={edit}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleChange(event)
+          }
+        />
+      </div>
+
+      <div className="textDiv">
+        <p className="social">Confirm Password : </p>
+        <TextField
+          sx={sx}
+          id="standard-required"
+          variant="standard"
+          size="medium"
+          value={userInfo.surname}
+          InputProps={{ disableUnderline: true }}
+          disabled={edit}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleChange(event)
+          }
+        />
+      </div>
+      <Button
+        sx={{
+          position: "absolute",
+        }}
+        size="large"
+        variant="contained"
+        endIcon={<SendIcon />}
+        disabled={edit}
+        onClick={updatePassword}
+      >
         SAVE
+      </Button>
+      <Button
+        sx={{
+          position: "absolute",
+        }}
+        variant="contained"
+        size="large"
+        onClick={handleEditableChange}
+        endIcon={<EditIcon />}
+      >
+        EDIT
       </Button>
     </>
   );
