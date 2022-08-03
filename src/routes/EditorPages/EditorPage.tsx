75a04bfacb4b9b412b71a24ad54e825786a899ce
Aycan <aycnkya12@gmail.com>
2022-08-03 17:34:27 +0300

    editor pages

---
+++ src/routes/EditorPages/EditorPage.tsx
@@ -0,0 +1,13 @@
+import ResponsiveAppBar from "./SetPostElements/EditorResponsiveBar";
+import SettingPosts from "./SetPostElements/SettingPosts";
+
+const EditorPage = () => {
+  return (
+    <>
+      <ResponsiveAppBar />
+      <SettingPosts />
+    </>
+  );
+};
+
+export default EditorPage;
