66c8dc43b430babb49652dd945454ed038fd256a
Aycan <aycnkya12@gmail.com>
2022-08-08 16:43:28 +0300

    home page edited

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,6 +1,12 @@
+import Posts from "./Elements/Posts";
 import HomeResponsiveBar from "./HomeResponsiveBar";
-
+import "./style.css";
 const Home = () => {
-  return <HomeResponsiveBar />;
+  return (
+    <>
+      <HomeResponsiveBar />
+      <Posts />
+    </>
+  );
 };
 export default Home;
