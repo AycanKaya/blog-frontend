1b20bca67f53f80b7299a757f4c2ad2f8297c721
Aycan <aycnkya12@gmail.com>
2022-08-09 11:05:33 +0300

    post sharing added

--- src/routes/BlogPage/BlogForm/SharePost/Elements/SharePosts.tsx
+++ src/routes/BlogPage/BlogForm/SharePost/Elements/SharePosts.tsx
@@ -1,13 +1,45 @@
-import { Card, Typography } from "@mui/material";
+import { Alert, Box, Card, Fab, Typography } from "@mui/material";
 import * as React from "react";
 import TextField from "@mui/material/TextField";
+import EditIcon from "@mui/icons-material/Edit";
+import { post } from "../../../../../api/axios";
+import Modal from "@mui/material/Modal";
+interface IPost {
+  title: string;
+  content: string;
+}
 
 export default function PostSharing() {
-  const [value, setValue] = React.useState("Controlled");
+  const [open, setOpen] = React.useState(false);
+  const [openError, setOpenError] = React.useState(false);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(event.target.value);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const handleOpenError = () => setOpenError(true);
+  const handleCloseError = () => setOpenError(false);
+
+  const [postContent, setPostContent] = React.useState<IPost>({
+    title: "",
+    content: "",
+  });
+  const handleChangeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPostContent({ ...postContent, title: event.target.value });
   };
+  const handleChangeContent = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPostContent({ ...postContent, content: event.target.value });
+  };
+
+  function handleClickPost() {
+    post("/Post/PostUser", postContent).then((response) => {
+      console.log(response);
+      if (response.succeeded) {
+        setPostContent({ title: "", content: "" });
+        return handleOpen();
+      }
+      handleOpenError();
+    });
+  }
 
   const sx = {
     boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
@@ -18,7 +50,34 @@ export default function PostSharing() {
     marginTop: "50px",
     padding: "10px",
   };
-
+  const style = {
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    color: "#8a6d3b",
+    bgcolor: "#fcf8e3",
+    opacity: 1,
+    border: "1px solid #d6e9c6",
+    borderRadius: "4px",
+    boxShadow: 24,
+    p: 4,
+  };
+  const styleSucceed = {
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    color: "3c763d",
+    bgcolor: "#dff0d8",
+    opacity: "1",
+    border: "1px solid #d6e9c6",
+    borderRadius: "4px",
+    boxShadow: 24,
+    p: 4,
+  };
   return (
     <>
       <Card sx={sx}>
@@ -26,17 +85,59 @@ export default function PostSharing() {
           fullWidth
           id="outlined-multiline-static"
           multiline
+          sx={{ marginTop: "7px", marginBottom: "7px" }}
           rows={1}
-          defaultValue="Enter a title"
+          placeholder="Enter a title"
+          value={postContent.title}
+          onChange={handleChangeTitle}
         />
         <TextField
           fullWidth
           id="outlined-multiline-static"
           multiline
+          sx={{ marginTop: "7px", marginBottom: "7px" }}
           rows={4}
-          defaultValue="Default Value"
+          placeholder="Content"
+          value={postContent.content}
+          onChange={handleChangeContent}
         />
+        <Fab
+          variant="extended"
+          sx={{ float: "right" }}
+          onClick={handleClickPost}
+        >
+          Post
+        </Fab>
       </Card>
+      <Modal
+        keepMounted
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="keep-mounted-modal-title"
+        aria-describedby="keep-mounted-modal-description"
+      >
+        <Box sx={styleSucceed}>
+          <Typography id="keep-mounted-modal-description" sx={{ mt: 2 }}>
+            <strong>Success!</strong> Your post has been sent to the Editor
+            user. The editor will be open to everyone when the user approves.
+          </Typography>
+        </Box>
+      </Modal>
+
+      <Modal
+        keepMounted
+        open={openError}
+        onClose={handleCloseError}
+        aria-labelledby="keep-mounted-modal-title2"
+        aria-describedby="keep-mounted-modal-description2"
+      >
+        <Box sx={style}>
+          <Typography id="keep-mounted-modal-description2" sx={{ mt: 2 }}>
+            <strong>Warning!</strong> An error occurred while sending your post
+            to the Editor user. Please try again.
+          </Typography>
+        </Box>
+      </Modal>
     </>
   );
 }
