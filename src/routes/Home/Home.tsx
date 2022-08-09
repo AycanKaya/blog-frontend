218d730d3390678af058bc8dbc61b9fee3dfa068
Aycan <aycnkya12@gmail.com>
2022-08-09 11:07:27 +0300

    tag search added to home page

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,10 +1,12 @@
 import Posts from "./Elements/Posts";
+import Search from "./Elements/Search";
 import HomeResponsiveBar from "./HomeResponsiveBar";
 import "./style.css";
 const Home = () => {
   return (
     <>
       <HomeResponsiveBar />
+      <Search />
       <Posts />
     </>
   );
