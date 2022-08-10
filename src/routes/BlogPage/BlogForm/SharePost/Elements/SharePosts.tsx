30a0c1b6591bb35a2328558d0d631013690974cd
Aycan <aycnkya12@gmail.com>
2022-08-10 23:03:55 +0300

    added library and editable

--- src/routes/BlogPage/BlogForm/SharePost/Elements/SharePosts.tsx
+++ src/routes/BlogPage/BlogForm/SharePost/Elements/SharePosts.tsx
@@ -1,8 +1,53 @@
-import { Alert, Box, Card, Fab, Typography } from "@mui/material";
+import {
+  alpha,
+  Box,
+  Card,
+  Fab,
+  InputBase,
+  styled,
+  Typography,
+} from "@mui/material";
 import * as React from "react";
-import TextField from "@mui/material/TextField";
 import { post } from "../../../../../api/axios";
 import Modal from "@mui/material/Modal";
+
+const BootstrapInput = styled(InputBase)(({ theme }) => ({
+  "label + &": {
+    marginTop: theme.spacing(3),
+  },
+  "& .MuiInputBase-input": {
+    borderRadius: 4,
+    position: "relative",
+    backgroundColor: theme.palette.mode === "light" ? "#fcfcfb" : "#2b2b2b",
+    border: "1px solid #ced4da",
+    fontSize: 16,
+    width: "974px",
+    padding: "10px 12px",
+    transition: theme.transitions.create([
+      "border-color",
+      "background-color",
+      "box-shadow",
+    ]),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+    "&:focus": {
+      boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+      borderColor: theme.palette.primary.main,
+    },
+  },
+}));
+
 interface IPost {
   title: string;
   content: string;
@@ -83,7 +128,7 @@ export default function PostSharing() {
   return (
     <>
       <Card sx={sx}>
-        <TextField
+        <BootstrapInput
           fullWidth
           id="outlined-multiline-static"
           multiline
@@ -93,7 +138,7 @@ export default function PostSharing() {
           value={postContent.title}
           onChange={handleChangeTitle}
         />
-        <TextField
+        <BootstrapInput
           fullWidth
           id="outlined-multiline-static"
           multiline
