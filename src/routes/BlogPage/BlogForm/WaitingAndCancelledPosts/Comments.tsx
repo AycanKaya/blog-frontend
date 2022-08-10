7252da3d4a3f6a6a2dd8d289e9d4c3d65371f636
Aycan <aycnkya12@gmail.com>
2022-08-10 18:01:55 +0300

    Comment system added

--- src/routes/BlogPage/BlogForm/WaitingAndCancelledPosts/Comments.tsx
+++ src/routes/BlogPage/BlogForm/WaitingAndCancelledPosts/Comments.tsx
@@ -5,12 +5,16 @@ import {
   CardContent,
   CardHeader,
   Typography,
+  TextField,
+  Button,
 } from "@mui/material";
 import { red } from "@mui/material/colors";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { useEffect, useState } from "react";
+import { post } from "../../../../api/axios";
 interface IComment {
   id: number;
   postID: number;
@@ -19,10 +23,21 @@ interface IComment {
   created: Date;
 }
 interface Props {
+  postId: number;
   comments: IComment[];
+  getRecentFivePosts: () => void;
+}
+interface requestBody {
+  content: string;
+  postID: number;
 }
 
-const Comments: React.FC<Props> = ({ comments }) => {
+const Comments: React.FC<Props> = ({
+  postId,
+  comments,
+  getRecentFivePosts,
+}) => {
+  const [reply, setReply] = useState("");
   const commentList = comments.map((comment: IComment) => (
     <Card
       sx={{
@@ -57,6 +72,30 @@ const Comments: React.FC<Props> = ({ comments }) => {
     </Card>
   ));
 
+  const [body, setBody] = useState<requestBody>({
+    content: "",
+    postID: postId,
+  });
+
+  const onClick = () => {
+    post("/Comment/ShareComment", body)
+      .then((response) => {
+        console.log(response);
+        console.log(body);
+        setReply("");
+        getRecentFivePosts();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleChange = (event: any) => {
+    setReply(event.target.value);
+    setBody({ ...body, content: reply });
+    console.log(postId, "veee");
+  };
+
   const sx = {
     marginTop: "20px",
     border: "none",
@@ -75,6 +114,28 @@ const Comments: React.FC<Props> = ({ comments }) => {
         <Typography>Comments</Typography>
       </AccordionSummary>
       <AccordionDetails>{commentList}</AccordionDetails>
+      <TextField
+        fullWidth
+        multiline
+        placeholder="your reply"
+        id="fullWidth"
+        value={reply}
+        onChange={handleChange}
+      />
+      <Button
+        sx={{
+          float: "right",
+          margin: "5px",
+          background: "rgb(120, 86, 255)",
+          color: "#ffff",
+          borderWidth: "1px",
+          borderRadius: "9999px",
+          fontFamily: "Verdana, Geneva, Tahoma, sans-serif",
+        }}
+        onClick={onClick}
+      >
+        Reply
+      </Button>
     </Accordion>
   );
 };
