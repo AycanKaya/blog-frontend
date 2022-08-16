4d17054e852fb8a2fe84b5f13eb44ae1814ec12f
Aycan <aycnkya12@gmail.com>
2022-08-16 16:12:23 +0300

    Theme created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import { BrowserRouter } from "react-router-dom";
-import Routes from "./routes";
+import { createTheme, Theme, ThemeProvider } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+
+import './App.css';
+
+const theme: Theme = createTheme({
+  palette: {
+    primary: {
+      main: '#3B719F',
+      contrastText: '#fafdff'
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000'
+    }
+  }
+});
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
