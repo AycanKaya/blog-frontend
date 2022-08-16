0edf77a7b88373ee44650cc00a42174203819a03
Aycan <aycnkya12@gmail.com>
2022-08-16 16:19:01 +0300

    mui job tabs removed

--- src/routes/BlogPage/BlogForm/WaitingAndCancelledPosts/AllPosts.tsx
+++ src/routes/BlogPage/BlogForm/WaitingAndCancelledPosts/AllPosts.tsx
@@ -1,15 +1,12 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Tab from "@mui/material/Tab";
-import TabContext from "@mui/lab/TabContext";
-import TabList from "@mui/lab/TabList";
-import TabPanel from "@mui/lab/TabPanel";
-import WaitingPosts from "./WaitingPosts";
-import CancelledPosts from "./CancelledPosts";
-import UserPosts from "./UserPosts";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { Tab, Tabs } from '@mui/material';
+import WaitingPosts from './WaitingPosts';
+import CancelledPosts from './CancelledPosts';
+import UserPosts from './UserPosts';
 
 export default function AllPosts() {
-  const [value, setValue] = React.useState("1");
+  const [value, setValue] = React.useState('1');
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
@@ -18,42 +15,31 @@ export default function AllPosts() {
   return (
     <Box
       sx={{
-        width: "100%",
+        width: '100%',
 
-        marginTop: "50px",
-        typography: "body1",
-      }}
-    >
-      <TabContext value={value}>
-        <Box
-          sx={{
-            borderBottom: 1,
-            borderColor: "#ffff",
-          }}
-        >
-          <TabList
-            sx={{ borderColor: "#5989b4a9" }}
-            onChange={handleChange}
-            textColor="secondary"
-            indicatorColor="secondary"
-            aria-label="lab API tabs example"
-            centered
-          >
-            <Tab label="Shared Posts" value="1" />
-            <Tab label="Waiting Posts" value="2" />
-            <Tab label="Cancelled Posts" value="3" />
-          </TabList>
-        </Box>
-        <TabPanel value="1">
-          <UserPosts />
-        </TabPanel>
-        <TabPanel value="2">
-          <WaitingPosts />
-        </TabPanel>
-        <TabPanel value="3">
-          <CancelledPosts />
-        </TabPanel>
-      </TabContext>
+        marginTop: '50px',
+        typography: 'body1'
+      }}>
+      <Box
+        sx={{
+          borderBottom: 1,
+          borderColor: '#ffff'
+        }}>
+        <Tabs
+          sx={{ borderColor: '#5989b4a9' }}
+          onChange={handleChange}
+          textColor="secondary"
+          indicatorColor="secondary"
+          aria-label="lab API tabs example"
+          centered>
+          <Tab label="Shared Posts" value="1" />
+          <Tab label="Waiting Posts" value="2" />
+          <Tab label="Cancelled Posts" value="3" />
+        </Tabs>
+      </Box>
+      {value === '1' && <UserPosts />}
+      {value === '2' && <WaitingPosts />}
+      {value === '3' && <CancelledPosts />}
     </Box>
   );
 }
