39929fea225401e88f92bfd6f64936e9db136738
Aycan <aycnkya12@gmail.com>
2022-08-16 16:21:58 +0300

    added

---
+++ src/routes/BlogPage/BlogForm/HomePage/PostTags.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { useState, useEffect } from "react";
+import ITag from "../../../../api/model/tag";
+import { getWithout } from "../../../../api/axios";
+import "./style.css";
+
+interface Props {
+  postId: number;
+}
+
+const PostTags: React.FC<Props> = ({ postId }) => {
+  const [tags, setTags] = useState<ITag[]>([]);
+
+  function getTags() {
+    getWithout("/Tag/GetTagsInPost?PostID=" + postId).then((response) => {
+      console.log("POSTTAG", response);
+      setTags(response.tags);
+    });
+  }
+  useEffect(() => {
+    getTags();
+  }, [postId]);
+
+  const TagList = tags.map((tag: ITag) => (
+    <div className="box">{tag.tagName}</div>
+  ));
+
+  return (
+    <>
+      <div>{TagList}</div>
+    </>
+  );
+};
+export default PostTags;
