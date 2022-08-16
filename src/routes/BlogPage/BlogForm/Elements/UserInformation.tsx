0aea3ecac30f7e15c23e83ae7339d29c46dd133f
Aycan <aycnkya12@gmail.com>
2022-08-16 16:14:54 +0300

    removed mui joy and changed tabs

--- src/routes/BlogPage/BlogForm/Elements/UserInformation.tsx
+++ src/routes/BlogPage/BlogForm/Elements/UserInformation.tsx
@@ -1,18 +1,15 @@
-import Box from "@mui/material/Box";
+import Box from '@mui/material/Box';
 
-import Tab from "@mui/material/Tab";
-import TabContext from "@mui/lab/TabContext";
-import TabList from "@mui/lab/TabList";
-import TabPanel from "@mui/lab/TabPanel";
-import ProfileSettings from "./ProfileSettings";
-import AccountSettings from "./AccountSettings";
-import { get, post } from "../../../../api/axios";
-import React, { useEffect, useState } from "react";
-import ProfileIcons from "./ProfileIcons";
-import { Button } from "@mui/material";
-import SaveButton from "./SaveButton";
-import EditButton from "./EditButton";
-import "./style.css";
+import { Tabs, Tab } from '@mui/material';
+import ProfileSettings from './ProfileSettings';
+import AccountSettings from './AccountSettings';
+import { get, post } from '../../../../api/axios';
+import { useEffect, useState } from 'react';
+import ProfileIcons from './ProfileIcons';
+import { Button } from '@mui/material';
+import SaveButton from './SaveButton';
+import EditButton from './EditButton';
+import './style.css';
 
 interface IUser {
   userID: string;
@@ -29,29 +26,29 @@ interface IUser {
   address: string;
 }
 const defaultUser: IUser = {
-  userID: "",
-  userName: "",
-  name: "",
-  surname: "",
-  email: "",
-  role: "",
+  userID: '',
+  userName: '',
+  name: '',
+  surname: '',
+  email: '',
+  role: '',
   gender: 0,
-  birthDay: new Date(""),
-  age: "",
-  phoneNumber: "",
-  contry: "",
-  address: "",
+  birthDay: new Date(''),
+  age: '',
+  phoneNumber: '',
+  contry: '',
+  address: ''
 };
 
 const UserInformaiton: React.FC = () => {
-  const [value, setValue] = useState("1");
+  const [value, setValue] = useState('1');
 
   const [edit, setEdit] = useState(true);
 
   const [userInfo, setUserInfo] = useState<IUser>(defaultUser);
 
   function getUserInfo() {
-    get("/Account/GetUserInfo").then((response: any) => {
+    get('/Account/GetUserInfo').then((response: any) => {
       setUserInfo(response.userInfo);
     });
   }
@@ -66,44 +63,34 @@ const UserInformaiton: React.FC = () => {
 
   return (
     <>
-      <ProfileIcons userName={userInfo.userName + " " + userInfo.surname} />
+      <ProfileIcons userName={userInfo.userName + ' ' + userInfo.surname} />
       <p className="email">{userInfo.email} </p>
       <Box
         sx={{
-          width: "auto",
-          typography: "body1",
-          alignItems: "center",
-          paddingLeft: "600px",
-          paddingTop: "25px",
-          display: "inline-table",
-          verticalAlign: "baseline",
-        }}
-      >
-        <TabContext value={value}>
-          <Box
-            sx={{
-              borderBottom: 1,
-              borderColor: "divider",
-              width: "600px",
-            }}
-          >
-            <TabList onChange={handleChange} aria-label="lab API tabs example">
-              <Tab label="User Information Settings" value="1" />
-              <Tab label="Account Settings" value="2" />
-            </TabList>
-          </Box>
-          <TabPanel value="1">
-            <ProfileSettings
-              userInfo={userInfo}
-              editable={edit}
-              setUserInfo={setUserInfo}
-            />
-          </TabPanel>
+          width: 'auto',
+          typography: 'body1',
+          alignItems: 'center',
+          paddingLeft: '600px',
+          paddingTop: '25px',
+          display: 'inline-table',
+          verticalAlign: 'baseline'
+        }}>
+        <Box
+          sx={{
+            borderBottom: 1,
+            borderColor: 'divider',
+            width: '600px'
+          }}>
+          <Tabs onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="User Information Settings" value="1" />
+            <Tab label="Account Settings" value="2" />
+          </Tabs>
+        </Box>
 
-          <TabPanel value="2">
-            <AccountSettings userInfo={userInfo} />
-          </TabPanel>
-        </TabContext>
+        {value === '1' && (
+          <ProfileSettings userInfo={userInfo} editable={edit} setUserInfo={setUserInfo} />
+        )}
+        {value === '2' && <AccountSettings userInfo={userInfo} />}
       </Box>
     </>
   );
