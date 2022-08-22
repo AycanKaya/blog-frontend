156fe177e40234fbe335a549b1bc652fa54f796d
Aycan <aycnkya12@gmail.com>
2022-08-22 17:54:22 +0300

    new route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,19 @@
-import { Routes as BaseRoutes, Route } from "react-router-dom";
+import { Routes as BaseRoutes, Route } from 'react-router-dom';
 
-import Home from "./Home";
-import Login from "./Login";
-import { Register } from "./Register";
-import BlogPage from "./BlogPage";
-import AdminPage from "./AdminPage";
-import SettingsPage from "./AdminPage/SettingsPage.tsx";
-import EditorPage from "./EditorPages";
-import EditorSettingsPage from "./EditorPages/SettingsPage/Settings";
-import SharePost from "./BlogPage/BlogForm/SharePost";
-import UserHomePage from "./BlogPage/BlogForm/HomePage";
-import EditorHomePage from "./EditorPages/EditorHomePage";
-import WaitingAndCancelledPosts from "./BlogPage/BlogForm/WaitingAndCancelledPosts";
+import Home from './Home';
+import Login from './Login';
+import { Register } from './Register';
+import BlogPage from './BlogPage';
+import AdminPage from './AdminPage';
+import SettingsPage from './AdminPage/SettingsPage.tsx';
+import EditorPage from './EditorPages';
+import EditorSettingsPage from './EditorPages/SettingsPage/Settings';
+import SharePost from './BlogPage/BlogForm/SharePost';
+import UserHomePage from './BlogPage/BlogForm/HomePage';
+import EditorHomePage from './EditorPages/EditorHomePage';
+import WaitingAndCancelledPosts from './BlogPage/BlogForm/WaitingAndCancelledPosts';
+import Callback from './CallbackPage/Callback';
+import ProfilePage from './ProfilePage/ProfilePage';
 
 export default function Routes() {
   return (
@@ -29,6 +31,8 @@ export default function Routes() {
         <Route path="basicUserHomePage" element={<UserHomePage />} />
         <Route path="editorHomePage" element={<EditorHomePage />} />
         <Route path="waitingPosts" element={<WaitingAndCancelledPosts />} />
+        <Route path="callback" element={<Callback />} />
+        <Route path="profile" element={<ProfilePage />} />
       </Route>
     </BaseRoutes>
   );
