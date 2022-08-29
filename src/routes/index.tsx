ef887e6201f3774aafe98013d71f57a4c8f7f5ee
Aycan <aycnkya12@gmail.com>
2022-08-29 18:09:37 +0300

    Routers updated

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,13 @@ import { Routes as BaseRoutes, Route, Outlet } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
 import { Register } from './Register';
-import UserHomePage from './UserHome';
 import ResponsiveAppBar from '../components/AppBar';
 import UserSettings from './UserSettings';
 import Dashboard from './Dashboard';
-
 import SettingPosts from './PostSettings';
 import SharePost from './SharePost';
+import Post from './Post/Post';
+import HomeUser from './HomeUser';
 
 export default function Routes() {
   return (
@@ -28,7 +28,8 @@ export default function Routes() {
         <Route path="userSettings" element={<UserSettings />} />
         <Route path="editorPostSettings" element={<SettingPosts />} />
         <Route path="sharePost" element={<SharePost />} />
-        <Route path="home" element={<UserHomePage />} />
+        <Route path="home" element={<HomeUser />} />
+        <Route path="post/:postId" element={<Post />} />
       </Route>
     </BaseRoutes>
   );
