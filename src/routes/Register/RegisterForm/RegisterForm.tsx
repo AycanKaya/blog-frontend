878384f9f2afe8a1510d293daba1c1c0589c60d0
Aycan <aycnkya12@gmail.com>
2022-09-05 16:46:32 +0300

    link added

--- src/routes/Register/RegisterForm/RegisterForm.tsx
+++ src/routes/Register/RegisterForm/RegisterForm.tsx
@@ -123,7 +123,7 @@ export default function RegisterForm(props: RegisterProps) {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="../login" variant="body2">
                   Already have an account? Sign in
                 </Link>
               </Grid>
